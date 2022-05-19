853539bbc14362231441f613b4e539a07c61626b
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-19 08:12:53 -0700

    Create Safari_Music.js

---
+++ Safari_Music.js
@@ -0,0 +1,13 @@
+from safarimusic 
+from utils.safaribot
+from Clinets telethon.pytgcalls
+               SAFARI_MUSIC == M"
+               MUSIC(on).abouse, Legend-MusicBot
+               safari_on = int(Mod_on)str(Legend-MusicBot)).os.event("Mod+not+run+string-session=")
+  safari_Qranifa==*(str,)*
+                          print("(event:)")>/git//github.com/SATYA-OP/SATYA-MUSIC/root/musicbot.RUN=RUN
+                           print(&),
+                                 &=*legend*legend1*legend2*legend*legend*(legend"legend=run,Run await: 
+                                                                          retune:
+                                                                          await:legend-musicbot
+                                                                          None,[error]==fixing_+op+=From(git/github.com/SATYA-OP/SATYA-MUSIC/gitclone,
