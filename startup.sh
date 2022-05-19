8df8329e88cfc947c6637b3f565452d4b59f4c08
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-19 06:58:36 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/LEGEND-AI/LEGEND-MUSIC /LEGEND-MUSIC
+git clone https://github.com/SATYA-OP/SATYA-MUSIC /SATYA-MUSIC
 echo ">> INSTALLING REQUIREMENTS..."
-cd /LEGEND-MUSIC
+cd /SATYA-MUSIC
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
