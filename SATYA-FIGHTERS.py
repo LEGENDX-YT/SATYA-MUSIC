46ba1f408990d484c9af8eb8db44391778f81687
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-06-02 08:45:35 -0700

    Create SATYA-FIGHTERS.py

---
+++ SATYA-FIGHTERS.py
@@ -0,0 +1,20 @@
+from hackers darkwed None, await input 
+(input)"string"hack.py
+            input==legendbot("hack")
+            print(hack)
+                       hackers("STRING_SESSION")await outside,
+                 await mode_on=False<
+                       mode_oof=true<
+                  await: legendbot(ON)
+      try() 
+           pass:LEGENDBOT()
+                                  HACKERS(await:(true)).outside),
+                                  mirrore_mod==X 
+                                  mirror_mod==Y 
+                                  legend=(int(.op)str(("await.print))).
+                                  legend("@mirrose_bot")
+                                  legend(print.userhack.)os
+                                  hack.os
+    pass:
+    try()legendhacker
+    #MADRCHODO JADA UDOGE TO PILLKE PADE RHOGE INSTA PAR TG PAR APNI OKAD ME HI RHO #
